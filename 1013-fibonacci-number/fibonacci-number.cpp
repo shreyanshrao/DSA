a223e11ad1cb961431b3328dd1803c52981a94a7
Shreyansh Rao <shreyanshrao13@gmail.com>
2025-08-29 18:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.63%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,8 +2,13 @@ class Solution {
 private:
 int possible(int n,vector<int>&dp){
     if(n<=1) return n;
-    if(dp[n]!=-1) return dp[n];
-    return dp[n]=possible(n-1,dp)+possible(n-2,dp);
+    dp[0]=0;
+    dp[1]=1;
+    // if(dp[n]!=-1) return dp[n];
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+     return dp[n];
     // if(n<=1) return n;
     // int last=possible(n-1);
     // int slast=possible(n-2);
@@ -12,7 +17,7 @@ int possible(int n,vector<int>&dp){
 }
 public:
     int fib(int n) {
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n+1);
         return possible(n,dp);
     }
 };
